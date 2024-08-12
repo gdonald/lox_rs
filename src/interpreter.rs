02f70cbea275d80b4f14363fd3bbf00ec97f51b4
Greg Donald <gd@darkclear.net>
2024-08-12 12:18:35 -0500

    add more operations

--- src/interpreter.rs
+++ src/interpreter.rs
@@ -14,12 +14,28 @@ impl Interpreter {
         if object.is::<Option<()>>() {
             return false;
         }
-        
+
         if let Some(boolean) = object.downcast_ref::<bool>() {
             return *boolean;
         }
-        
+
         true
+    }
+
+    fn is_equal(&self, a: &Object, b: &Object) -> bool {
+        if a.is::<f64>() && b.is::<f64>() {
+            return a.downcast_ref::<f64>() == b.downcast_ref::<f64>();
+        }
+
+        if a.is::<String>() && b.is::<String>() {
+            return a.downcast_ref::<String>() == b.downcast_ref::<String>();
+        }
+
+        if a.is::<bool>() && b.is::<bool>() {
+            return a.downcast_ref::<bool>() == b.downcast_ref::<bool>();
+        }
+
+        false
     }
 }
 
@@ -31,40 +47,95 @@ impl ExprVisitor<Object> for Interpreter {
     fn visit_binary_expr(&mut self, expr: &BinaryExpr) -> Object {
         let left = self.evaluate(Expr::from(*expr.left.clone()));
         let right = self.evaluate(Expr::from(*expr.right.clone()));
-        
+
         match expr.operator.token_type {
+            TokenType::EqualEqual => {
+                let value = self.is_equal(&left, &right);
+                Object::new(value)
+            }
+            TokenType::BangEqual => {
+                let value = self.is_equal(&left, &right);
+                Object::new(!value)
+            }
+            TokenType::Greater => {
+                if let (Some(left_num), Some(right_num)) =
+                    (left.downcast_ref::<f64>(), right.downcast_ref::<f64>())
+                {
+                    Object::new(left_num > right_num)
+                } else {
+                    panic!("Operands must be numbers for the GREATER operation");
+                }
+            }
+            TokenType::GreaterEqual => {
+                if let (Some(left_num), Some(right_num)) =
+                    (left.downcast_ref::<f64>(), right.downcast_ref::<f64>())
+                {
+                    Object::new(left_num >= right_num)
+                } else {
+                    panic!("Operands must be numbers for the GREATER EQUAL operation");
+                }
+            }
+            TokenType::Less => {
+                if let (Some(left_num), Some(right_num)) =
+                    (left.downcast_ref::<f64>(), right.downcast_ref::<f64>())
+                {
+                    Object::new(left_num < right_num)
+                } else {
+                    panic!("Operands must be numbers for the LESS operation");
+                }
+            }
+            TokenType::LessEqual => {
+                if let (Some(left_num), Some(right_num)) =
+                    (left.downcast_ref::<f64>(), right.downcast_ref::<f64>())
+                {
+                    Object::new(left_num <= right_num)
+                } else {
+                    panic!("Operands must be numbers for the LESS EQUAL operation");
+                }
+            }
             TokenType::Plus => {
-                if let (Some(left_num), Some(right_num)) = (left.downcast_ref::<f64>(), right.downcast_ref::<f64>()) {
+                if let (Some(left_num), Some(right_num)) =
+                    (left.downcast_ref::<f64>(), right.downcast_ref::<f64>())
+                {
                     Object::new(left_num + right_num)
-                } else if let (Some(left_str), Some(right_str)) = (left.downcast_ref::<String>(), right.downcast_ref::<String>()) {
+                } else if let (Some(left_str), Some(right_str)) = (
+                    left.downcast_ref::<String>(),
+                    right.downcast_ref::<String>(),
+                ) {
                     Object::new(format!("{}{}", left_str, right_str))
                 } else {
                     panic!("Operands must be matching types for the PLUS operation");
                 }
             }
             TokenType::Minus => {
-                if let (Some(left_num), Some(right_num)) = (left.downcast_ref::<f64>(), right.downcast_ref::<f64>()) {
+                if let (Some(left_num), Some(right_num)) =
+                    (left.downcast_ref::<f64>(), right.downcast_ref::<f64>())
+                {
                     Object::new(left_num - right_num)
                 } else {
                     panic!("Operands must be numbers for the MINUS operation");
                 }
             }
             TokenType::Slash => {
-                if let (Some(left_num), Some(right_num)) = (left.downcast_ref::<f64>(), right.downcast_ref::<f64>()) {
+                if let (Some(left_num), Some(right_num)) =
+                    (left.downcast_ref::<f64>(), right.downcast_ref::<f64>())
+                {
                     Object::new(left_num / right_num)
                 } else {
                     panic!("Operands must be numbers for the SLASH operation");
                 }
             }
             TokenType::Star => {
-                if let (Some(left_num), Some(right_num)) = (left.downcast_ref::<f64>(), right.downcast_ref::<f64>()) {
+                if let (Some(left_num), Some(right_num)) =
+                    (left.downcast_ref::<f64>(), right.downcast_ref::<f64>())
+                {
                     Object::new(left_num * right_num)
                 } else {
                     panic!("Operands must be numbers for the STAR operation");
                 }
             }
             _ => {
-                panic!("Unknown operator");  // Handle other operators or errors
+                panic!("Unknown operator"); // Handle other operators or errors
             }
         }
     }
@@ -73,7 +144,6 @@ impl ExprVisitor<Object> for Interpreter {
         let value = self.evaluate(Expr::from(*expr.expr.clone()));
         Object::new(value)
     }
-
 
     fn visit_unary_expr(&mut self, expr: &UnaryExpr) -> Object {
         let value = self.evaluate(Expr::from(*expr.right.clone()));
@@ -86,9 +156,7 @@ impl ExprVisitor<Object> for Interpreter {
                     panic!("Operand must be a number");
                 }
             }
-            TokenType::Bang => {
-                Object::new(!self.is_truthy(&value))
-            }
+            TokenType::Bang => Object::new(!self.is_truthy(&value)),
             _ => {
                 panic!("Unknown token type");
             }
