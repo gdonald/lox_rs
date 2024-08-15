2560e060b2f1c92ae1a3f3d89e11b3d441bb8a40
Greg Donald <gd@darkclear.net>
2024-08-15 17:26:50 -0500

    add more tests

---
+++ tests/expr.rs
@@ -0,0 +1,29 @@
+use lox_rs::ast::expr::{Expr, BinaryExpr, UnaryExpr, LiteralExpr, GroupingExpr};
+
+#[test]
+fn test_create_literal_expr_with_num() {
+    let literal = Expr::Literal(Box::new(LiteralExpr::Num(17.0)));
+    if let Expr::Literal(lit) = literal {
+        if let LiteralExpr::Num(value) = *lit {
+            assert_eq!(value, 17.0);
+        } else {
+            panic!("Expected LiteralExpr::Num");
+        }
+    } else {
+        panic!("Expected Expr::Literal");
+    }
+}
+
+#[test]
+fn test_create_literal_expr_with_str() {
+    let literal = Expr::Literal(Box::new(LiteralExpr::Str(String::from("Hello"))));
+    if let Expr::Literal(lit) = literal {
+        if let LiteralExpr::Str(value) = *lit {
+            assert_eq!(value, "Hello");
+        } else {
+            panic!("Expected LiteralExpr::Str");
+        }
+    } else {
+        panic!("Expected Expr::Literal");
+    }
+}
