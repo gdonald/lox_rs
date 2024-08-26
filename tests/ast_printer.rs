872909d19bef3d745f27afd918995c1bfc906a86
Greg Donald <gd@darkclear.net>
2024-08-26 14:53:33 -0500

    add some test for ast printer visitor

--- tests/ast_printer.rs
+++ tests/ast_printer.rs
@@ -1,6 +1,6 @@
-use lox_rs::ast::expr::{Expr, BinaryExpr, UnaryExpr, LiteralExpr, GroupingExpr};
-use lox_rs::ast::token::{Token, TokenType};
+use lox_rs::ast::expr::{BinaryExpr, Expr, GroupingExpr, LiteralExpr, UnaryExpr, Visitor};
 use lox_rs::ast::printer::Printer;
+use lox_rs::ast::token::{Token, TokenType};
 
 #[test]
 fn test_ast_printer() {
@@ -29,4 +29,44 @@ fn test_ast_printer() {
     let result = printer.print(&expression);
 
     assert_eq!(result, "(* (- 123) (group 45.67))");
+}
+
+#[test]
+fn test_visit_literal_str() {
+    let expr = LiteralExpr::Str("Hello".to_string());
+    let mut printer = Printer;
+    let result = printer.visit_literal_expr(&expr);
+    assert_eq!(result, "Hello");
+}
+
+#[test]
+fn test_visit_literal_num() {
+    let expr = LiteralExpr::Num(42.0);
+    let mut printer = Printer;
+    let result = printer.visit_literal_expr(&expr);
+    assert_eq!(result, "42");
+}
+
+#[test]
+fn test_visit_literal_bool_true() {
+    let expr = LiteralExpr::Bool(true);
+    let mut printer = Printer;
+    let result = printer.visit_literal_expr(&expr);
+    assert_eq!(result, "true");
+}
+
+#[test]
+fn test_visit_literal_bool_false() {
+    let expr = LiteralExpr::Bool(false);
+    let mut printer = Printer;
+    let result = printer.visit_literal_expr(&expr);
+    assert_eq!(result, "false");
+}
+
+#[test]
+fn test_visit_literal_nil() {
+    let expr = LiteralExpr::Nil;
+    let mut printer = Printer;
+    let result = printer.visit_literal_expr(&expr);
+    assert_eq!(result, "nil");
 }
